c7f3f7134855db215f3b5a26b8e7658b55cbe32e
oscargeovanny.rincon <oscargeovannyrincon@gmail.com>
2024-05-15 00:11:54 -0500

    Reto_02 difilcultad ++

---
+++ reto_02-1.py
@@ -0,0 +1,9 @@
+'''/*
+* DIFICULTAD EXTRA (opcional):
+ * Crea un programa que imprima por consola todos los números comprendidos
+ * entre 10 y 55 (incluidos), pares, y que no son ni el 16 ni múltiplos de 3.
+ *
+ * Seguro que al revisar detenidamente las posibilidades has descubierto algo nuevo.
+ */'''
+numbers=list(range(10,56))
+print(numbers)
