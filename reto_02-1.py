82f5b0fde1661738cf046435ed0bc50ea771d551
oscargeovanny.rincon <oscargeovannyrincon@gmail.com>
2024-05-15 08:40:35 -0500

    Reto_02-1 numero de 10 a 55, pares, que no sea el 16 ni multiplos de 3

--- reto_02-1.py
+++ reto_02-1.py
@@ -6,4 +6,10 @@
  * Seguro que al revisar detenidamente las posibilidades has descubierto algo nuevo.
  */'''
 numbers=list(range(10,56))
-print(numbers)
+#print(numbers)
+for number in numbers:
+    if number%2==0:
+        if number!=16:
+            if number%3!=0:
+                print(number)
+                
